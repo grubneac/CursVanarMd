d727b3143f876041b2a6a98ecddaafadb118962f
SASHA <SASHA@GAZ>
2019-09-28 18:06:43 +0300

    рефакторинг библиотеки

--- StudentDatabase/src/helpers/Validator.java
+++ StudentDatabase/src/helpers/Validator.java
@@ -3,37 +3,28 @@ package helpers;
 public class Validator {
 	// - возраст ( валидация: min 18 .. max 65 )
 	public static boolean checkAge( int age,int min,int max ) {
-		boolean result = false;
-		if(age>=min&&age<=max)result=true;
-		return result;
+		return age>=min&&age<=max;
 	}
 	
 	public static boolean checkExperience( int experience,int min,int max ) {
-		boolean result = false;
-		if(experience>=min && experience<=max)result=true;
-		return result;
+		return experience>=min && experience<=max;
 
 	}
 	// - имя/фамилия ( валидация: min 5.. max 20 символов, обязательное присутствие 1 пробела не ближе чем 2 знака от каждого края)
 	public static boolean checkPersonName( String personName,int min,int max ) {
-		boolean result = false;
-		if (personName.length()>=min 
+		return (personName.length()>=min 
 				&& personName.length()<=max
 				&& personName.indexOf(' ')>1 // в начале первых два знака не должны быть пробелами
 				&& personName.lastIndexOf(' ')<personName.length()-2 //последние два знака не должны быть пробелами
-				
-				)result = true;
-		return result;
+				);
 	}
 
 	public static boolean checkMin(Integer experience, int minExperience) {
-		if (experience>=minExperience)return true;
-		return false;
+		return experience>=minExperience;
 	}
 //средняя годовая оценка ( валидация: 1.0 .. 10.0 )
 	public static boolean checkMark( float mark,float min,float max ) {
-		if(mark>=min && mark<=max)return true;
-		return false;
+		return mark>=min && mark<=max;
 
 	}
 	
