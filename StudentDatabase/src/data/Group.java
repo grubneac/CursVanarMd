f295885fad03f4711fdb5aed3e64ae4483959add
grubnyak <a.grubnyak@cft.ru>
2019-09-30 15:40:00 +0300

    удаление и добавление студента в группу

--- StudentDatabase/src/data/Group.java
+++ StudentDatabase/src/data/Group.java
@@ -1,6 +1,6 @@
 package data;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 
 import exceptions.NullException;
 import exceptions.OutOfRangeException;
@@ -9,13 +9,13 @@ import helpers.Validator;
 public class Group {
 	String name;// - имя группы ( валидация: min 5 .. max 30 )
 	Integer year;// - год / курс ( валидация: min 1 .. max 7 )
-	ArrayList<Student> students;// - ссылка на список студентов группы
+	HashSet<Student> students;// - ссылка на список студентов группы
 	private final int minNameLength = 5;
 	private final int maxNameLength = 30;
 	private final int minYear = 1;
 	private final int maxYear = 7;
 	
-	public Group(String name, Integer year, ArrayList<Student> students) throws OutOfRangeException {
+	public Group(String name, Integer year, HashSet<Student> students) throws OutOfRangeException {
 		super();
 		if(Validator.checkRange(name.length(), minNameLength, maxNameLength))
 			this.name = name;
@@ -41,10 +41,10 @@ public class Group {
 			this.year = year;
 		else throw new OutOfRangeException("год / курс:"+year+" ( валидация: min "+minYear+" .. max "+maxYear+" )");
 	}
-	public ArrayList<Student> getStudents() {
+	public HashSet<Student> getStudents() {
 		return students;
 	}
-	public void setStudents(ArrayList<Student> students) {
+	public void setStudents(HashSet<Student> students) {
 		this.students = students;
 	}
 	@Override
@@ -54,17 +54,17 @@ public class Group {
 	
 	public boolean addStudent(Student student) throws NullException {
 		if(Validator.isNotNull(student)) {
-			//TODO не допускать дублирования. Меняем на HashSet/ Переопределить equal and hashcode  в Student(имя и возраст)
-			//TODO при добавлении студента в группу, у студента прописываем группу
+			
 			students.add(student);
+			//при добавлении студента в группу, у студента прописываем группу
+			student.setGroup(this);
 			return true;
 			}
 		else throw new NullException("Нельзя добавлять null студента");
 			
 	}
 	public boolean removeStudent(Student student) throws NullException {
-		//TODO при удаление студента из группы, у студента удаляем группу
-		//student.setGroup(null);
+		student.removeGroup();
 		return students.remove(student);
 	}
 	
