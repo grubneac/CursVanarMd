b672c52ed028f2e5bc3a9e7ae28e5bdfffea4892
grubnyak <a.grubnyak@cft.ru>
2019-10-10 15:01:22 +0300

    task issue 6
    https://github.com/dorinesinenco/database-students-oop/issues/6

---
+++ ConcurrencyAPI/src/TwoThreads.java
@@ -0,0 +1,65 @@
+
+public class TwoThreads {
+
+	public static void main(String[] args) {
+			new Incrementer().start();
+			new Decrementer().start();
+			new Printer().start();
+	}
+
+}
+class Box {
+	   public static Integer value = 0;
+	}
+
+	class Incrementer extends Thread{
+	       public void increment(){
+	             if (Box.value <3)
+	             Box.value++;
+	       }
+
+		@Override
+		public void run() {
+			while(true) {
+			increment();
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}			}
+		}
+	       
+	}
+	class Decrementer extends Thread{
+	       public void decrement(){
+	             if(Box.value > -3)
+	             Box.value--;
+	       }
+			@Override
+			public void run() {
+				while(true) {
+				decrement();
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}				}
+			}
+	}
+	class Printer extends Thread{
+	       public void print(){
+	             if (Box.value>=0) System.out.println( Box.value );
+	             else System.err.println(Box.value);
+	       }
+			@Override
+			public void run() {
+				while(true) {
+				print();
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				}
+			}
+	}
