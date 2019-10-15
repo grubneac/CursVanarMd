ab85f4b695dd312a83b793b485753cc62d634641
SASHA <SASHA@GAZ>
2019-10-15 23:40:18 +0300

    реализовал задачу по валютам на дату
    https://github.com/dorinesinenco/EDUQATION/blob/master/programming/java/public.api/coinlayer/coin.provider.ru.md

--- WatherApp/src/CoinRateProvider.java
+++ WatherApp/src/CoinRateProvider.java
@@ -1,6 +1,9 @@
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Date;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -11,7 +14,7 @@ import org.json.JSONObject;
 
 public class CoinRateProvider {
 	private static final String  ENDPOINT ="http://api.coinlayer.com/api/live?access_key=e86330f4310b780fc3ebac7b9a6bc617"; 
-	
+	private static String CurENDPOINT = ENDPOINT;
 	/**
 	 * endpoint: Reques https://api.coinlayer.com/live
 	 * возвращает карту где код валюты - ключ, а коэфициент - значение
@@ -19,7 +22,7 @@ public class CoinRateProvider {
 	 * @throws MalformedURLException 
 	 * */
 	public HashMap<String,Float>  getLiveRates() throws MalformedURLException, IOException {
-		Scanner scaner = new Scanner(new URL(ENDPOINT).openStream());
+		Scanner scaner = new Scanner(new URL(CurENDPOINT).openStream());
 		String  json_str =scaner.nextLine();
 		scaner.close();
 	
@@ -31,7 +34,6 @@ public class CoinRateProvider {
 		
 		HashMap<String,Float> resultMap = new HashMap<>();
 		for (Map.Entry<String, Object> pair : hashMap.entrySet()) {
-			System.out.println(pair.getKey()+" : "+Float.parseFloat(pair.getValue().toString()));
 			resultMap.put(pair.getKey(), Float.parseFloat(pair.getValue().toString()));
 		}
 		return resultMap;
@@ -45,23 +47,31 @@ public class CoinRateProvider {
 	 * @throws MalformedURLException 
 	 * */
 	public HashMap<String,Float>  getLiveRates(HashSet<String> currencies) throws MalformedURLException, IOException {
-		HashMap<String,Float> hashMap = getLiveRates();
-		HashMap<String,Float> resultMap = new HashMap<>();
-		for (Map.Entry<String, Float> pair : hashMap.entrySet()) {
-			if (currencies.contains(pair.getKey())){
-				resultMap.put(pair.getKey(), pair.getValue());
-			}
-		}
-		return resultMap;
+		CurENDPOINT = ENDPOINT;
+		CurENDPOINT = SetCurEndPointByCodeVal(currencies, CurENDPOINT);
+//		System.out.println(CurENDPOINT);
+		
+		HashMap<String,Float> hashMap =getLiveRates();
+		CurENDPOINT = ENDPOINT;
+		return hashMap;
 	}
 	
 	
 	/**
 	 * endpoint: https://api.coinlayer.com/YYYY-MM-DD
 	 * возвращает карту где код валюты - ключ, а коэфициент - значение - за определенную дату
+	 * @throws IOException 
+	 * @throws MalformedURLException 
 	 * */	
-	public HashMap<String,Float>  getHistoricalRates() {
-		return null;
+	public HashMap<String,Float>  getHistoricalRates(Date date) throws MalformedURLException, IOException {
+		SimpleDateFormat df = new SimpleDateFormat("YYYY-MM-DD");
+		CurENDPOINT = ENDPOINT;
+		CurENDPOINT = CurENDPOINT.replace("live", df.format(date));
+//		System.out.println(CurENDPOINT);
+		
+		HashMap<String,Float> hashMap =getLiveRates();
+		CurENDPOINT = ENDPOINT;
+		return hashMap;
 		
 	}	
 
@@ -69,10 +79,34 @@ public class CoinRateProvider {
 	 * endpoint: https://api.coinlayer.com/YYYY-MM-DD
 	 * возвращает карту где код валюты - ключ, а коэфициент - значение - за определенную дату
 	 * параметр "currencies" служит фильтром (мы передаем коды тех валют что нас интересуют) 
+	 * @throws IOException 
+	 * @throws MalformedURLException 
 	 * */	
-	public HashMap<String,Float>  getHistoricalRates(HashSet<String> currencies) {
-		return null;
+	public HashMap<String,Float>  getHistoricalRates(HashSet<String> currencies, Date date) throws MalformedURLException, IOException {
+		SimpleDateFormat df = new SimpleDateFormat("YYYY-MM-DD");
+		CurENDPOINT = ENDPOINT;
+		CurENDPOINT = CurENDPOINT.replace("live", df.format(date));
+		CurENDPOINT = SetCurEndPointByCodeVal(currencies, CurENDPOINT);
+//		System.out.println(CurENDPOINT);
+		
+		HashMap<String,Float> hashMap =getLiveRates();
+		CurENDPOINT = ENDPOINT;
+		return hashMap;
 		
 	}	
+	
+	private static String SetCurEndPointByCodeVal(HashSet<String> currencies,String vPoint) {
+		Boolean flag =false;
+		String resultPoint=vPoint;
+		resultPoint +="&&symbols=";
+		for (String code : currencies) {
+			if(flag) {//первый раз запятая не нужна
+				resultPoint +=",";
+			}
+			resultPoint +=code;
+			flag =true;
+		}		
+		return resultPoint;		
+	}
 	
 }
--- WatherApp/src/TestApplication.java
+++ WatherApp/src/TestApplication.java
@@ -1,7 +1,10 @@
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -11,7 +14,7 @@ import org.json.JSONObject;
 
 public class TestApplication {
 
-	public static void main(String[] args) throws MalformedURLException, IOException {
+	public static void main(String[] args) throws MalformedURLException, IOException, ParseException {
 //		OpenWeatherProvider openWeatherProvider = new OpenWeatherProvider();
 //		System.out.println("Chisinau temp="+openWeatherProvider.getCurrentTemperature("Chisinau"));
 //		System.out.println("London temp="+openWeatherProvider.getCurrentTemperature("London"));
@@ -20,19 +23,36 @@ public class TestApplication {
 		CoinRateProvider coinRateProvider = new CoinRateProvider();
 		
 //		-------------------------------------------------------------------------------
-		HashMap<String, Float> hashMap= coinRateProvider.getLiveRates();
-		System.out.println("function getLiveRates return:");
-		for (Map.Entry<String, Float> pair	: hashMap.entrySet()) {
-			System.out.println(pair.getKey()+" : "+String.format("%.7f", pair.getValue()));
-		}
-//		-------------------------------------------------------------------------------
+//		HashMap<String, Float> hashMap= coinRateProvider.getLiveRates();
+//		System.out.println("function getLiveRates return:");
+//		for (Map.Entry<String, Float> pair	: hashMap.entrySet()) {
+//			System.out.println(pair.getKey()+" : "+String.format("%.7f", pair.getValue()));
+//		}
+////		-------------------------------------------------------------------------------
 		HashSet<String> codVal = new HashSet<>(Arrays.asList("MDA","LUX","EUR"));
 		HashMap<String, Float> hashMap2= coinRateProvider.getLiveRates(codVal);
-		System.out.println("function getLiveRates return:");
+		System.out.println("function getLiveRates2 return:");
 		for (Map.Entry<String, Float> pair	: hashMap2.entrySet()) {
 			System.out.println(pair.getKey()+" : "+String.format("%.7f", pair.getValue()));
 		}
 //		-------------------------------------------------------------------------------
+//		SimpleDateFormat df= new SimpleDateFormat("dd.mm.yyyy");
+//		Date dat = df.parse("15.05.2019");
+//		HashMap<String, Float> hashMap3= coinRateProvider.getHistoricalRates(dat);
+//		System.out.println("function getLiveRates3 return:");
+//		for (Map.Entry<String, Float> pair	: hashMap3.entrySet()) {
+//			System.out.println(pair.getKey()+" : "+String.format("%.7f", pair.getValue()));
+//		}
+//		-------------------------------------------------------------------------------
+		SimpleDateFormat df= new SimpleDateFormat("dd.mm.yyyy");
+		Date dat = df.parse("15.05.2019");
+		HashMap<String, Float> hashMap3= coinRateProvider.getHistoricalRates(codVal, dat);
+		System.out.println("function getLiveRates4 return:");
+		for (Map.Entry<String, Float> pair	: hashMap3.entrySet()) {
+			System.out.println(pair.getKey()+" : "+String.format("%.7f", pair.getValue()));
+		}
+///		-------------------------------------------------------------------------------
+		
 	}
 
 }
