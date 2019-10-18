d81f45d6e9f037fddbb5291989b23247f7183462
grubnyak <a.grubnyak@cft.ru>
2019-10-18 11:50:08 +0300

    Join Thread Example

---
+++ ConcurrencyAPI/src/JoinThreadExample.java
@@ -0,0 +1,42 @@
+
+public class JoinThreadExample {
+
+	public static void main(String[] args) throws InterruptedException {
+		Thread th1 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println(Thread.currentThread().getName());
+				
+			}
+		});
+		Thread th2 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println(Thread.currentThread().getName());
+				
+			}
+		});
+		
+		th1.start();
+		th2.start();
+		
+		th1.join();
+		
+		System.out.println("End main Thread");
+
+	}
+
+}
