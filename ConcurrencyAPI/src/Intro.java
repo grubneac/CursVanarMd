76cc55862bb0c3171923fc3ab540da10d711ecc8
grubnyak <a.grubnyak@cft.ru>
2019-10-18 09:12:01 +0300

    Два потока бьются за один ресурс. Рабочий вариант.

---
+++ ConcurrencyAPI/src/Intro.java
@@ -0,0 +1,39 @@
+
+public class Intro {
+
+	public static void main(String[] args) {
+		Thread in1 = new Thread(new Incrementals("Thread 1"));
+		Thread in2 = new Thread(new Incrementals("Thread 2"));
+		
+		in1.start();
+		in2.start();
+		
+
+	}
+
+}
+
+class Incrementals implements Runnable {
+	private static volatile int counter = 0;
+	private String name;
+	private static final Object monitor = new Object();
+	
+	public Incrementals(String name) {
+		super();
+		this.name = name;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < 1_000_000; i++) {
+			increment();			
+		}
+		System.out.println(name+" : "+counter);
+	}
+	
+	private void increment() {
+		synchronized (monitor) {
+			counter++;
+		}
+	}
+}
