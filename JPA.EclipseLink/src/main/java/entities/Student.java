b5bb66cc194754ddf0bea440a517f211ff8829b7
SASHA <SASHA@GAZ>
2019-11-19 20:46:03 +0200

    STEP.4.ru.md  add, update, find record by JPA
    https://github.com/dorinesinenco/EDUQATION/blob/master/programming/java/database/JPA.PG.Students/STEP.4.ru.md

--- JPA.EclipseLink/src/main/java/entities/Student.java
+++ JPA.EclipseLink/src/main/java/entities/Student.java
@@ -1,23 +1,47 @@
 package entities;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table
 public class Student {
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+//	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long Id;
 	private String fullName;
 	
+	public Student() {
+		super();
+	}
+	
+	public Student(Long id, String fullName) {
+		super();
+		Id = id;
+		this.fullName = fullName;
+	}
+
 	public String getFullName() {
 		return fullName;
 	}
 	public void setFullName(String fullName) {
 		this.fullName = fullName;
 	}
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [Id=" + Id + ", fullName=" + fullName + "]";
+	}
+	
 
 }
--- JPA.EclipseLink/src/main/java/main/Application.java
+++ JPA.EclipseLink/src/main/java/main/Application.java
@@ -1,12 +1,31 @@
 package main;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+
+import org.eclipse.persistence.sessions.Session;
+
+import entities.Student;
 
 public class Application {
 
 	public static void main(String[] args) {
-		check();
+//		ArrayList<Student> students = new ArrayList<Student>();
+		install();// create table
+		
+		Student student = new Student(11L, "John Doe");
+		Student returnStudent = findOneStudent(student.getId());
+		if (returnStudent ==null) createStudent(student);
+		else updateStudent(student);
+		
+		System.out.println(returnStudent);
+		
+		student = new Student(12L, "Vasea Pecus");
+		returnStudent = findOneStudent(student.getId());
+		if (returnStudent == null) createStudent(student);
+		else updateStudent(student);
+		System.out.println(returnStudent);
 	}
 	
 	public static void check(){
@@ -14,5 +33,47 @@ public class Application {
 		var em =entityManagerFactory.createEntityManager();
 		em.close();
 	}
+	 public static void install() {
+	       EntityManagerFactory factory = Persistence.createEntityManagerFactory("sqlite-database");
+	       EntityManager em = factory.createEntityManager();
+	       
+	       em.getTransaction().begin();
 
+	       em
+	         .createNativeQuery("Create table if not exists Student(id INTEGER PRIMARY KEY, fullName VARCHAR(30));")
+	         .executeUpdate();
+
+	       em.getTransaction().commit();
+	       
+	   }
+	 public static Student findOneStudent(Long id) {
+	       EntityManagerFactory factory = Persistence.createEntityManagerFactory("sqlite-database");
+	       EntityManager em = factory.createEntityManager();
+	       
+	       em.getTransaction().begin();
+	       
+	       Student s = em.find(Student.class,id);
+
+	       em.getTransaction().commit();
+	       return s;
+	   }
+	 public static void createStudent(Student student) {
+	       EntityManagerFactory factory = Persistence.createEntityManagerFactory("sqlite-database");
+	       EntityManager em = factory.createEntityManager();
+	       
+	       em.getTransaction().begin();
+	       	em.persist(student);
+	       em.getTransaction().commit();
+	      
+	   }
+	 public static void updateStudent(Student student) {
+	       EntityManagerFactory factory = Persistence.createEntityManagerFactory("sqlite-database");
+	       EntityManager em = factory.createEntityManager();
+	       
+	       em.getTransaction().begin();
+	       	em.merge(student);
+	       em.getTransaction().commit();
+	      
+	   }	 
+	 
 }
